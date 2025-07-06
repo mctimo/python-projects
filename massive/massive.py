57632e024ae4590b7998be0f9fe5fc85d4ade5ab
artem.ostapenko <artem.ostapenko@ringcentral.com>
2025-07-06 19:21:50 +0200

    added insert method

--- massive/massive.py
+++ massive/massive.py
@@ -10,7 +10,7 @@ class Massive():
         if self.lenght < self.cap:
             self.massive[self.lenght] = x
             self.lenght += 1
-        if self.lenght >= self.cap:
+        if self.lenght == self.cap:
             self.cap *= 2
             new_massive = (ctypes.c_int * self.cap)()
             for i in range(self.lenght):
@@ -19,6 +19,24 @@ class Massive():
 
             self.massive = new_massive
 
+    def insert(self, index, x):
+            if index >= self.lenght:
+                self.append(x)
+                return
+
+            if self.lenght == self.cap:
+                self.cap *= 2
+                new_massive = (ctypes.c_int * self.cap)()
+                for i in range(self.lenght):
+                    new_massive[i] = self.massive[i]
+                self.massive = new_massive
+
+            for i in range(self.lenght, index, -1):
+                self.massive[i] = self.massive[i - 1]
+
+            self.massive[index] = x
+            self.lenght += 1
+
     def __getitem__(self, index):
         return self.massive[index]
 
@@ -26,11 +44,18 @@ class Massive():
         return  self.lenght
 
 mass = Massive()
-
-for i in range(15):
+for i in range(40):
     mass.append(i)
+
 
 for i in range(len(mass)):
     print(mass[i])
 
+
 # add insert вставление элемента перед номером
+
+#
+# Если длина меньше капасити
+# Меняем элемент на тот индекс который передаем
+# Все что справа от него двигаем вправо
+# Если длина равна или больше капасити
