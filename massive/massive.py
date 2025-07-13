0328be9f735edb9528cfb85477fc36701b98ea57
artem.ostapenko <artem.ostapenko@ringcentral.com>
2025-07-13 11:11:52 +0200

    added mul imul iter str add iadd

--- massive/massive.py
+++ massive/massive.py
@@ -21,7 +21,6 @@ class Massive():
         if self.lenght == self.cap:
             self.resizing()
 
-
     def insert(self, index, x):
             if index >= self.lenght:
                 self.append(x)
@@ -36,16 +35,99 @@ class Massive():
             self.massive[index] = x
             self.lenght += 1
 
+    def __iter__(self):
+        for i in range(self.lenght):
+            yield self.massive[i]
+
     def __getitem__(self, index):
         return self.massive[index]
 
     def __len__(self):
         return  self.lenght
 
-mass = Massive()
-for i in range(40):
-    mass.append(i)
+    def __str__(self):
+        string = '['
+        for i in range(self.lenght):
+            if i == 0:
+                string += f'{str(self[i])}'
+            else:
+                string += f', {str(self[i])}'
+        string += ']'
+        return string
+
+    def __add__(self, other):
+        new_mass = Massive()
+        for i in self:
+            new_mass.append(i)
+        for i in other:
+            new_mass.append(i)
+        return new_mass
+    
+    def __iadd__(self, other):
+        for i in other:
+            self.append(i)
+        return self
+
+    def __mul__(self, num):
+        new_mass = Massive()
+        for i in range(num):
+            new_mass += self
+        return new_mass
+
+    def __imul__(self, num):
+        new_mass = []
+        for i in range(num):
+            new_mass += self
+        return new_mass
 
 
-for i in range(len(mass)):
-    print(mass[i])
+# ДЗ: реализовать все оставшиеся методы массива:
+# __add__ - done
+# __class__ - skip
+# __class_getitem__ - skip
+# __contains__ - ?
+# __delattr__
+# __delitem__
+# __dir__
+# __doc__
+# __eq__
+# __format__
+# __ge__
+# __getattribute__
+# __getitem__ - done
+# __getstate__
+# __gt__
+# __hash__
+# __iadd__ - done
+# __imul__ - done
+# __init__ - done
+# __init_subclass__ - skip
+# __iter__ - done
+# __le__
+# __len__ - done
+# __lt__
+# __mul__ - done
+# __ne__
+# __new__
+# __reduce__
+# __reduce_ex__
+# __repr__
+# __reversed__
+# __rmul__
+# __setattr__
+# __setitem__
+# __sizeof__
+# __str__ - done
+# __subclasshook__
+# append
+# clear
+# copy
+# count
+# extend
+# index
+# insert
+# pop
+# remove
+# reverse
+# sort
+
